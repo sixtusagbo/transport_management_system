898b4ebdf4b39f3e19773d1d36f4f0413386a3d3
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2021-10-31 18:17:22 +0100

    updated models and migrations
    added passenger details and created driver model with -m

--- database/migrations/2021_10_31_101858_create_drivers_table.php
+++ database/migrations/2021_10_31_101858_create_drivers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateDriversTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('drivers', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email')->unique();
+            $table->date('dob');
+            $table->text('address');
             $table->string('phone_number');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->tinyInteger('type')->nullable()->default(0);
-            $table->rememberToken();
+            $table->tinyText('state');
+            $table->tinyText('lga');
+            $table->string('experience');
             $table->timestamps();
         });
     }
@@ -34,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('drivers');
     }
 }
