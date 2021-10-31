b5c17098cfc1e07dcaa00195b4afbe333dbf6a0c
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2021-10-31 02:24:35 +0100

    updated app name and user model
    updated user model and tested it by adding a new user using tinker

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('phone_number');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('user_type')->nullable()->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
