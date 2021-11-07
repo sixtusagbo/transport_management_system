a176e82025542b4c6fb258aab07f484461c54c81
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2021-11-07 20:49:19 +0100

    update on landing page
    removed sign in from top right

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -112,11 +112,6 @@
 							</div>
 							<div class="col-xl-2 col-lg-3 text-lg-left text-xl-right d-none d-lg-block">
 								<div class="header_phone">
-									<a href="{{ url('login')}}">
-										<span>Sign In</span>
-									</a>
-										
-									
 									<h6>
 										<span>1-800</span>-123-4567
 									</h6>
@@ -164,10 +159,10 @@
 													<p class="text-uppercase intro_after_featured_word">In God we trust</p>
 												</div>
 												<div class="intro_layer page-bottom animate" data-animation="pullDown">
-													<a class="btn btn-maincolor" href="about.html"> Book a Trip</a>
+													<a class="btn btn-maincolor" href="{{ route('login') }}"> Book a Trip</a>
 												</div>
 												<div class="intro_layer page-bottom animate" data-animation="pullDown">
-													<a class="btn btn-success" href="about.html"> Send Cargo </a>
+													<a class="btn btn-success" href="{{ route('login') }}"> Send Cargo </a>
 												</div>
 											</div>
 											
