ab93104fdf89f225666956df87019d525d9d0652
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2021-11-07 20:37:48 +0100

    clean up
    Removed favicon and other unnecessary shits

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,15 +7,13 @@
 <!--<![endif]-->
 
 <head>
-	<title>Home</title>
+	<title>Transport Mgt System | Home</title>
 	<meta charset="utf-8">
 	<!--[if IE]>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<![endif]-->
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
 	<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}">
 	<link rel="stylesheet" href="{{ asset('css/animations.css')}}">
