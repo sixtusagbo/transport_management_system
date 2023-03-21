cb77d69ef07eb1d1dac86dfaa47541b3c9b6b9e6
sixtusagbo <miracleagbosixtus@gmail.com>
2023-03-21 15:07:31 +0100

    Update dashboard layouts

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -27,7 +27,7 @@
   <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href="{{request()->url()}}">
+      <a class="navbar-brand m-0" href="/">
         <img src="{{ asset('images/peacelogo.png')}}" class="navbar-brand-img h-100 ms-5 center" alt="main_logo">
       </a>
     </div>
@@ -72,14 +72,6 @@
               <i class="fas fa-map-marker-alt" style="font-size:1.4em;"></i>
             </div>
             <span class="nav-link-text ms-1">Destinations</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white " href="{{request()->url()}}">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="fas fa-truck-container" style="font-size:1.4em;"></i>
-            </div>
-            <span class="nav-link-text ms-1">Cargo Bookings</span>
           </a>
         </li>
        
